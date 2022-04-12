d0741ed495e3a77937d9c276a614e0e72a265b45
dexcabigting <dexcabigting1234@gmail.com>
2022-04-12 22:40:35 +0800

    Updated App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,30 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 
 function App() {
+  const [backendData, setBackendData] = useState([{}]);
+
+  useEffect(() => {
+    fetch('/api').then(
+      response => response.json()
+    ).then(
+      data => {
+        setBackendData(data)
+      }
+    )
+  }, []);
+
   return (
-    <div>App</div>
+    <div>
+      {
+        (typeof backendData.employees === 'undefined') ? (
+          <p>Fetching...</p>
+        ) : (
+          backendData.employees.map((employee, key) => (
+            <p key={key}>{employee}</p>
+          ))
+        )
+      }
+    </div>
   )
 }
 
