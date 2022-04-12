e41725671d17a5e650ebec10a32f299d96dc241a
dexcabigting <dexcabigting1234@gmail.com>
2022-04-13 00:06:34 +0800

    Updated server.js

--- server/server.js
+++ server/server.js
@@ -5,9 +5,30 @@ app.get('/api', (req, res) => {
     res.json(
         {
             'employees': [
-                'employee1',
-                'employee2',
-                'employee3',
+                {
+                    'firstName': 'Pedro',
+                    'lastName': 'Batumbakal',
+                    'position': 'Mafia Boss',
+                    'sickLeaveCredits': 4,
+                    'vacationLeaveCredits': 2,                     
+                    'hourlyRate': 210
+                },
+                {
+                    'firstName': 'Lex',
+                    'lastName': 'Luthor',
+                    'position': 'CEO',
+                    'sickLeaveCredits': 3,
+                    'vacationLeaveCredits': 4,                     
+                    'hourlyRate': 220
+                },
+                {
+                    'firstName': 'Panny',
+                    'lastName': 'Macquiao',
+                    'position': 'Bakser',
+                    'sickLeaveCredits': 2,
+                    'vacationLeaveCredits': 7,                    
+                    'hourlyRate': 230
+                }
             ]
         }
     );
